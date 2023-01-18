5e4ff69f164d012aaf74bbf561f5943626187a3f
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-19 00:35:57 +0500

    find_all_users_id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -10,9 +10,23 @@ def find_all_users_id(data: dict)->list:
         list: List containing all the users id
     """
     messages=data['messages']
-    list1=[]
+    user_id=[]
     for i in messages:
-        list1.append(i['id'])
-    return list1
+        actor_id=i.get('actor_id')
+        from_id=i.get('from_id')
+
+        if from_id!=None:
+            is_channel='channel' in from_id
+
+            if not from_id in user_id and not is_channel:
+                user_id.append(from_id)
+        else:
+            is_channel='channel' in actor_id
+
+            if not actor_id in user_id and not is_channel:
+                user_id.append(actor_id)
+
+
+    return user_id
 x=read_data('data/result.json')
 print(find_all_users_id(x))
